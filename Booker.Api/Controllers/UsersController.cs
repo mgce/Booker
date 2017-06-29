4df9a78f1ce61bee49bf88071f69e0710f0a82f8
mgce <blogcontend@gmail.com>
2017-06-29 18:30:12 +0200

    Update Controller

---
+++ Booker.Api/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Booker.Infrastructure.Commands.Users;
+using Booker.Infrastructure.Services;
+
+namespace Booker.Api.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: api/Users
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // GET: api/Users/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Users
+        public async void Post([FromBody]CreateUserCommand command)
+        {
+            await _userService.RegisterAsync(command.Email, command.Username, command.Password);
+        }
+
+        // PUT: api/Users/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Users/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Booker.Tests.EndToEnd/Controllers/UsersControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+
+namespace Booker.Tests.EndToEnd.Controllers
+{
+    public class UsersControllerTests
+    {
+        private readonly HttpClient client;
+
+        public UsersControllerTests()
+        {
+            
+        }
+    }
+}
